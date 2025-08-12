6200ba98c266694b95dadce6ff26c487924309ab
talibDennis <joseph.dennis@triosstudent.com>
2025-08-12 12:43:28 -0400

    Added Sand stadium

---
+++ stadium/Sand.java
@@ -0,0 +1,19 @@
+package stadium;
+
+public class Sand extends Stadium {
+  private int dexterityCost = randNum.nextInt(6) + 7; // 7 - 12
+  private String type = "Sand";
+
+  public Sand() {
+    super();
+    super.setDexterityCost(this.dexterityCost);
+    super.setType(this.type);
+  } // constructor
+
+  public Sand(int dexterityCost) {
+    super();
+    super.setDexterityCost(this.dexterityCost);
+    super.setType(this.type);
+  } // constructor
+
+} // class
