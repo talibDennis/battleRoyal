f2ca93162b2ac3f4d9539d338f10b84ebbaedfd8
talibDennis <joseph.dennis@triosstudent.com>
2025-08-12 10:29:59 -0400

    Added Calm element to Weather

---
+++ weather/Calm.java
@@ -0,0 +1,19 @@
+package weather;
+
+public class Calm extends Weather {
+  private int dexterityCost = randNum.nextInt(3) + 1; // 1 2 3
+  private String type = "Calm";
+
+  public Calm() {
+    super();
+    super.setDexterityCost(this.dexterityCost);
+    super.setType(this.type);
+  } // constructor
+
+  public Calm(int dexterityCost) {
+    super();
+    super.setDexterityCost(this.dexterityCost);
+    super.setType(this.type);
+  } // construtor
+
+} // class
