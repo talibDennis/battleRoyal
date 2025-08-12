c23f6addb8fad22452149a7ec1127253c4fa65c5
talibDennis <joseph.dennis@triosstudent.com>
2025-08-12 12:52:27 -0400

    Added Hill stadium

---
+++ stadium/Hill.java
@@ -0,0 +1,19 @@
+package stadium;
+
+public class Hill extends Stadium {
+  private int dexterityCost = randNum.nextInt(3) + 4; // 4 - 6
+  private String type = "Hill";
+
+  public Hill() {
+    super();
+    super.setDexterityCost(this.dexterityCost);
+    super.setType(this.type);
+  } // constructor
+
+  public Hill(int dexterityCost) {
+    super();
+    super.setDexterityCost(this.dexterityCost);
+    super.setType(this.type);
+  } // constructor
+
+} // class
